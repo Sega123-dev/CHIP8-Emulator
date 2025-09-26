76e3579d9f8c9f2de3afa6424864967d671bb6fe
Sega <brajicsergej@gmail.com>
2025-09-26 22:16:26 +0200

    loadProgram() implemented

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdint>
+#include <fstream>
+#include <vector>
 
 class Chip8
 {
@@ -15,13 +17,14 @@ class Chip8
     uint8_t display[64][32]; // 64x32 px,scaled up to 10
 
     // Sets everything to 0 or default
+
     void initialize()
     {
         for (int i = 0; i < 16; i++)
             V[i] = 0;
         delay_timer = 0;
         sound_timer = 0;
-        pc = 0x200;
+        pc = 0x200; // Programs starts here
         for (int i = 0; i < 4096; i++)
             memory[i] = 0;
         I = 0;
@@ -31,9 +34,34 @@ class Chip8
     };
 
     // Loads the game into memory
-    void loadProgram(const std::string *filename) {};
+
+    void loadProgram(const char *filename)
+    {
+        std::ifstream rom(filename, std::ios::binary | std::ios::ate); // Sets it to binary type and we start reading the file from the end
+        if (rom.fail())
+        {
+            std::cerr << "Failed to load ROM: " << filename << std::endl;
+            return;
+        }
+        std::streamsize size = rom.tellg(); // Tells the file size
+        rom.seekg(0, std::ios::beg);        // Move pointer to beggining
+
+        std::vector<char> buffer(size); // Temporary array to store the file
+
+        if (rom.read(buffer.data(), size)) // Reads data
+        {
+            for (int i = 0; i < size; i++)
+            {
+                memory[0x200 + i] = static_cast<uint8_t>(buffer[i]);
+            }
+        }
+        rom.close(); // Closes the file
+
+        std::cout << "ROM dumped successfully!";
+    };
 
     // CH8 interpreter
+
     void interpreter() {};
 };
 
