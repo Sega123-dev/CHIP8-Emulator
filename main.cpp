90f64138f1dcaf047b6f85ace60aebaa8cb7c722
Sega <brajicsergej@gmail.com>
2025-10-06 21:34:06 +0200

    Chip8 finally showing something and works properly,not enough tested

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// No using winMain()
 #define SDL_MAIN_HANDLED
 
 #include <SDL2/SDL.h>
@@ -13,434 +14,279 @@ class Chip8
 {
 public:
     uint8_t memory[4096]; // RAM memory
-    uint8_t V[16];        // 8-bit registers from V0 - VF //VF - flag register
-    uint16_t I;           // Index register
-    uint16_t pc;          // Program counter
-    uint16_t stack[16];   // Call stack, used for doing sub-tasks
-    uint16_t sp;          // Stack pointer,points to the free space in stack memory
-    uint8_t delay_timer;  // Sets delays in the program, counts down at 60Hz
+    uint8_t V[16];        // Registers V0 - VF (16),VF-flag register that saves state and events
+    uint16_t I;           // Index pointer - holds adresses of sprites
+    uint16_t pc;          // Program counter - used for moving through instructions
+    uint16_t stack[16];   // Stack - holds memory adresses for doing sub-tasks
+    uint16_t sp;          // Stack pointer - used to point to a specific adress in stack
+    uint8_t delay_timer;
     uint8_t sound_timer;
-    uint8_t keypad[16];      // Keyboard input
-    uint8_t display[32][64]; // 32x64 px,scaled up to 10
-
-    // Sets everything to 0 or default
+    uint8_t keypad[16];
+    uint8_t display[32][64];
 
     void initialize()
     {
-        for (int i = 0; i < 16; i++)
-            V[i] = 0;
+        pc = 0x200; // All Chip8 programs start here
+        I = 0;
+        sp = 0;
         delay_timer = 0;
         sound_timer = 0;
-        pc = 0x200; // Programs starts here
-        for (int i = 0; i < 4096; i++)
-            memory[i] = 0;
-        I = 0;
-        for (int i = 0; i < 16; i++)
-            stack[i] = 0;
-        sp = 0;
+        srand(time(nullptr)); // For DRW
 
-        for (int y = 0; y < 32; y++)
-            for (int x = 0; x < 64; x++)
-                display[y][x] = 0;
+        // Clear memory, registers, display, stack
+        memset(memory, 0, sizeof(memory));
+        memset(V, 0, sizeof(V));
+        memset(stack, 0, sizeof(stack));
+        memset(display, 0, sizeof(display));
+        memset(keypad, 0, sizeof(keypad));
 
-        srand(time(nullptr)); // Generating a random number for RND Vx, byte
+        addFontset();
+    }
 
-        // Function logs
-
-        std::cout
-            << "RAM log: 0KB loaded\n";
-        for (int i = 0; i < 0xF + 1; i++)
-            std::cout << "Register V" << i << ":" << V[i] << '\n';
-        std::cout << "Delay timer: " << delay_timer << "Hz" << '\n';
-        std::cout << "Sound timer: " << sound_timer << '\n';
-        std::cout << "Program counter is set to: " << pc << '\n';
-        std::cout << "Index pointer: " << I << '\n';
-        for (int i = 0; i < 16; i++)
-            std::cout << "Stack #" << i + 1 << ":" << stack[i] << '\n';
-        std::cout << "Stack pointer: " << sp << '\n';
-    };
-
-    // Loads the game into memory
+    void addFontset()
+    {
+        // Tile system (5x4 pixels)
+        uint8_t chip8_fontset[80] = {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+        for (int i = 0; i < 80; i++)
+            memory[i] = chip8_fontset[i];
+    }
 
     void loadProgram(const char *filename)
     {
-        std::ifstream rom(filename, std::ios::binary | std::ios::ate); // Sets it to binary type and we start reading the file from the end
-        if (rom.fail())
+        std::ifstream rom(filename, std::ios::binary | std::ios::ate); // Transforms the file into raw binary and starts reading in from the end
+        if (!rom)
         {
             std::cerr << "Failed to load ROM: " << filename << std::endl;
             return;
         }
-        std::streamsize size = rom.tellg(); // Tells the file size
-        rom.seekg(0, std::ios::beg);        // Move pointer to beggining
 
-        std::vector<char> buffer(size); // Temporary array to store the file
+        std::streamsize size = rom.tellg(); // Tells the file size in bytes
+        rom.seekg(0, std::ios::beg);        // Goes on beggining for reading opcodes
 
-        if (rom.read(buffer.data(), size)) // Reads data
+        std::vector<char> buffer(size);    // Dynamic array,holds the file size so we can put the program in the RAM
+        if (rom.read(buffer.data(), size)) // Reads the file by its size
         {
             for (int i = 0; i < size; i++)
-            {
-                memory[0x200 + i] = static_cast<uint8_t>(buffer[i]);
-            }
+                memory[0x200 + i] = static_cast<uint8_t>(buffer[i]); // Puts the file in memory
         }
-        rom.close(); // Closes the file
+    }
 
-        // Logs
-
-        std::cout << "ROM dumped successfully!\n";
-        std::cout << size << "/" << "4096B loaded\n";
-    };
-
-    // CH8 interpreter
-
-    void emulateCycle()
+    void emulateCycle() // CHIP8 interpreter
     {
-        uint16_t opcode = memory[pc] << 8 | memory[pc + 1]; // Fetch opcodes(one is 2 bytes)
-
-        // Highest nibble
+        uint16_t opcode = memory[pc] << 8 | memory[pc + 1]; // Extracting the opcode
 
         switch (opcode & 0xF000)
         {
         case 0x0000:
-            switch (opcode & 0x00FF)
+            if ((opcode & 0x00FF) == 0x00E0)
             {
-            case 0x00E0: // CLS
-                for (int i = 0; i < 32; i++)
-                {
-                    for (int j = 0; j < 64; j++)
-                    {
-                        display[i][j] = 0;
-                    }
-                }
+                memset(display, 0, sizeof(display));
                 pc += 2;
-
-                std::cout << "Screen cleared\n";
-
-                break;
-            case 0x00EE: // RET: Return from subroutine
+            }
+            else if ((opcode & 0x00FF) == 0x00EE)
+            {
                 sp--;
                 pc = stack[sp];
-
-                std::cout << "[RET]Current instruction adress: " << pc << '\n';
-
-                break;
-            default:
-                std::cout << "Unknown 0x0000 opcode: " << std::hex << opcode << "\n";
-                break;
             }
+            else
+                pc += 2;
             break;
-
-        case 0x1000: // Jump to address
+        case 0x1000:
             pc = opcode & 0x0FFF;
-            std::cout << "Jumped to adress: " << pc << '\n';
             break;
-
-        case 0x2000: // Call Stack
+        case 0x2000:
             stack[sp] = pc + 2;
             sp++;
             pc = opcode & 0x0FFF;
-            std::cout << "New instruction placed in stack: " << stack[sp] << '\n';
             break;
-            // SE Vx byte
         case 0x3000:
         {
             uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t kk = opcode & 0x00FF;
-
-            if (V[x] == kk)
-            {
-                pc += 4; // Skip the next instruction
-                std::cout << "Opcode: " << opcode << ". Instruction skipped" << "\n";
-            }
-            else
-                pc += 2; // Move to next instruction
-            break;
-        }
-            // SNE Vx, byte
-        case 0x4000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t kk = opcode & 0x00FF;
-            if (V[x] != kk)
-            {
+            if (V[x] == (opcode & 0x00FF))
                 pc += 4;
-                std::cout << "Opcode: " << opcode << ". Instruction skipped" << "\n";
-            }
             else
                 pc += 2;
             break;
         }
-        // SNE Vx, byte
-        case 0x5000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t y = (opcode & 0x00F0) >> 4;
-            if (V[x] == V[y])
-            {
-                pc += 4;
-                std::cout << "Opcode: " << opcode << ". Instruction skipped" << "\n";
-            }
-            else
-            {
-                pc += 2;
-            }
-            break;
-        }
-        // LD Vx,byte
         case 0x6000:
         {
             uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t kk = opcode & 0x00FF;
-
-            V[x] = kk;
+            V[x] = opcode & 0x00FF;
             pc += 2;
             break;
         }
-        // ADD Vx,byte
         case 0x7000:
         {
             uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t kk = opcode & 0x00FF;
-
-            V[x] = V[x] + kk;
+            V[x] += opcode & 0x00FF;
             pc += 2;
             break;
         }
-        // Arithmetical/logical instructions
-        case 0x8000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t y = (opcode & 0x00F0) >> 4;
-            uint8_t operation = opcode & 0x000F;
-
-            switch (operation)
-            {
-            case 0x0:
-                V[x] = V[y];
-                pc += 2;
-                break;
-            case 0x1:
-                V[x] = V[x] | V[y];
-                pc += 2;
-                break;
-            case 0x2:
-                V[x] = V[x] & V[y];
-                pc += 2;
-                break;
-            case 0x3:
-                V[x] = V[x] ^ V[y];
-                pc += 2;
-                break;
-            case 0x4:
-            {
-                uint8_t sum = V[x] + V[y];
-                sum > 255 ? V[0xF] = 1 : V[0xF] = 0;
-                V[x] = sum & 0xFF;
-                pc += 2;
-                break;
-            }
-
-            case 0x5:
-                V[0xF] = (V[x] >= V[y]) ? 1 : 0;
-                V[x] = V[x] - V[y];
-                pc += 2;
-                break;
-            case 0x6:
-                V[0xF] = V[x] & 0x1; // LSB
-                V[x] = V[x] >> 1;
-                pc += 2;
-                break;
-            case 0x7:
-            {
-                V[0xF] = (V[y] >= V[x]) ? 1 : 0;
-                V[x] = V[y] - V[x];
-                pc += 2;
-                break;
-            }
-            case 0xE:
-                V[0xF] = (V[x] & 0x80) >> 7; // MSB
-                V[x] = V[x] << 1;
-                pc += 2;
-                break;
-            }
-            break;
-        }
-        // SNE Vx, Vy
-        case 0x9000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t y = (opcode & 0x00F0) >> 4;
-
-            if (V[x] != V[y])
-            {
-                pc += 4;
-                std::cout << "Opcode: " << opcode << ". Instruction skipped.";
-            }
-            else
-            {
-                pc += 2;
-            }
-            break;
-        }
-        // LD I, addr
         case 0xA000:
-        {
-            uint16_t address = opcode & 0x0FFF;
-            I = address;
-            break;
-        }
-        // JP V0, addr
-        case 0xB000:
-        {
-            uint16_t address = opcode & 0x0FFF;
-            pc = address + V[0];
-            break;
-        }
-        // RND Vx, byte
-        case 0xC000:
-        {
-            uint8_t random = rand() % 256;
-            uint8_t kk = opcode & 0x00FF;
-            uint8_t x = (opcode & 0x0F00) >> 8;
-
-            V[x] = random & kk;
+            I = opcode & 0x0FFF;
             pc += 2;
             break;
-        }
-        // DRW Vx, Vy, nibble
         case 0xD000:
         {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t y = (opcode & 0x00F0) >> 4;
-            uint8_t k = opcode & 0x000F;
-            uint8_t spriteRow = 0;
+            uint8_t x = V[(opcode & 0x0F00) >> 8];
+            uint8_t y = V[(opcode & 0x00F0) >> 4];
+            uint8_t height = opcode & 0x000F;
             V[0xF] = 0;
-            for (int i = 0; i < k; i++)
+
+            for (int row = 0; row < height; row++)
             {
-                spriteRow = memory[I + i];
-                for (int j = 0; j < 8; j++)
+                uint8_t sprite = memory[I + row];
+                for (int col = 0; col < 8; col++)
                 {
-                    uint8_t spriteBit = (spriteRow & (0x80 >> j)) ? 1 : 0;
-                    uint8_t &pixel = display[(V[y] + i) % 32][(V[x] + j) % 64]; // wrap around screen
-                    if (pixel == 1 && spriteBit == 1)
-                        V[0xF] = 1;
-                    pixel ^= spriteBit;
+                    if ((sprite & (0x80 >> col)) != 0)
+                    {
+                        int px = (x + col) % 64;
+                        int py = (y + row) % 32;
+                        if (display[py][px] == 1)
+                            V[0xF] = 1;
+                        display[py][px] ^= 1;
+                    }
                 }
             }
-            std::cout << "Sprite has been drawn!";
+
+            pc += 2;
+            break;
+        }
+        case 0xC000:
+        {
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            V[x] = (rand() % 256) & (opcode & 0x00FF);
             pc += 2;
             break;
         }
         case 0xE000:
         {
             uint8_t x = (opcode & 0x0F00) >> 8;
-            switch (opcode & 0x00FF)
+            if ((opcode & 0x00FF) == 0x9E)
             {
-            case 0x9E: // SKP Vx
                 if (keypad[V[x]])
-                {
                     pc += 4;
-                    std::cout << "Opcode: " << opcode << ". Instruction skipped.";
-                }
                 else
                     pc += 2;
-                break;
-
-            case 0xA1: // SKNP Vx
+            }
+            else if ((opcode & 0x00FF) == 0xA1)
+            {
                 if (!keypad[V[x]])
-                {
                     pc += 4;
-                    std::cout << "Opcode: " << opcode << ". Instruction skipped.";
-                }
                 else
                     pc += 2;
-                break;
             }
             break;
-        };
+        }
         default:
-            std::cout << "Unknown opcode: " << std::hex << opcode << '\n';
+            pc += 2;
             break;
         }
 
         // Timers
-
         if (delay_timer > 0)
             delay_timer--;
         if (sound_timer > 0)
         {
             sound_timer--;
             if (sound_timer == 0)
-            {
-                std::cout << "BEEP!" << "\n";
-                Beep(440, 1200);
-            }
+                Beep(440, 200);
         }
-    };
+    }
 };
 
 int main()
 {
     Chip8 chip8;
     chip8.initialize();
+    chip8.loadProgram("pong.ch8");
 
-    // Load the ROM (make sure pong.ch8 is in the same folder as executable)
-    chip8.loadProgram("chip8logo.ch8");
+    const int SCALE = 10;
+    const int CYCLES_PER_FRAME = 60; // Cycle function needs to be executed this many times in a frame so it can actually show something on the screen
+    const int FRAME_DELAY_MS = 16;   // 60 FPS
 
-    const int SCALE = 10;             // scale each CHIP-8 pixel
-    const int CYCLES_PER_FRAME = 100; // CHIP-8 instructions per frame (~10-15 is good)
-    const int FRAME_DELAY_MS = 16;    // ~60 FPS
+    // Makes the centered 64x32 window scaled by 10
 
     SDL_Window *window = SDL_CreateWindow("CHIP-8 Emulator",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
                                           64 * SCALE, 32 * SCALE,
                                           SDL_WINDOW_SHOWN);
+    // IDK NEXT
+
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Rect rect;
-    rect.w = SCALE;
-    rect.h = SCALE;
-
+    SDL_Rect rect{0, 0, SCALE, SCALE};
     bool running = true;
     SDL_Event e;
 
+    const SDL_Keycode keymap[16] = {
+        SDLK_x, SDLK_1, SDLK_2, SDLK_3,
+        SDLK_q, SDLK_w, SDLK_e, SDLK_a,
+        SDLK_s, SDLK_d, SDLK_z, SDLK_c,
+        SDLK_4, SDLK_r, SDLK_f, SDLK_v};
+
     while (running)
     {
-        // Handle events
+        // Event handling
         while (SDL_PollEvent(&e))
         {
             if (e.type == SDL_QUIT)
                 running = false;
-            // TODO: Map keyboard events to chip8.keypad[]
+
+            if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP)
+            {
+                bool pressed = e.type == SDL_KEYDOWN;
+                for (int i = 0; i < 16; i++)
+                    if (e.key.keysym.sym == keymap[i])
+                        chip8.keypad[i] = pressed;
+            }
         }
 
-        // Run a limited number of cycles per frame
+        // Run cycles
         for (int i = 0; i < CYCLES_PER_FRAME; i++)
             chip8.emulateCycle();
 
-        // Clear screen
+        // Render display
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Draw CHIP-8 display
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         for (int y = 0; y < 32; y++)
-        {
             for (int x = 0; x < 64; x++)
-            {
                 if (chip8.display[y][x])
                 {
                     rect.x = x * SCALE;
                     rect.y = y * SCALE;
                     SDL_RenderFillRect(renderer, &rect);
                 }
-            }
-        }
 
-        // Present frame
         SDL_RenderPresent(renderer);
-
-        // Wait for next frame (~60 FPS)
         SDL_Delay(FRAME_DELAY_MS);
     }
+
+    // Ends the SDL display process to save memory
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
