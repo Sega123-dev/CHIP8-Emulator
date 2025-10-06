643c3edf62700f92ac129e4cfb110fa95b6e4c28
Sega <brajicsergej@gmail.com>
2025-10-06 22:28:43 +0200

    CHIP8 emulator fully working,needed some basic optimization

--- main.cpp
+++ main.cpp
@@ -88,66 +88,133 @@ public:
         }
     }
 
-    void emulateCycle() // CHIP8 interpreter
+    void emulateCycle()
     {
-        uint16_t opcode = memory[pc] << 8 | memory[pc + 1]; // Extracting the opcode
+        uint16_t opcode = memory[pc] << 8 | memory[pc + 1]; // Fetch opcode
+
+        uint8_t x = (opcode & 0x0F00) >> 8;
+        uint8_t y = (opcode & 0x00F0) >> 4;
+        uint8_t kk = opcode & 0x00FF;
+        uint16_t nnn = opcode & 0x0FFF;
+        uint8_t n = opcode & 0x000F;
 
         switch (opcode & 0xF000)
         {
         case 0x0000:
-            if ((opcode & 0x00FF) == 0x00E0)
+            switch (opcode & 0x00FF)
             {
+            case 0xE0: // CLS
                 memset(display, 0, sizeof(display));
                 pc += 2;
-            }
-            else if ((opcode & 0x00FF) == 0x00EE)
-            {
+                break;
+            case 0xEE: // RET
                 sp--;
                 pc = stack[sp];
-            }
-            else
+                break;
+            default:
+                // SYS addr (ignored on modern interpreters)
                 pc += 2;
+                break;
+            }
             break;
-        case 0x1000:
-            pc = opcode & 0x0FFF;
+
+        case 0x1000: // JP addr
+            pc = nnn;
             break;
-        case 0x2000:
+
+        case 0x2000: // CALL addr
             stack[sp] = pc + 2;
             sp++;
-            pc = opcode & 0x0FFF;
+            pc = nnn;
             break;
-        case 0x3000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            if (V[x] == (opcode & 0x00FF))
-                pc += 4;
-            else
-                pc += 2;
+
+        case 0x3000: // SE Vx, byte
+            pc += (V[x] == kk) ? 4 : 2;
             break;
-        }
-        case 0x6000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            V[x] = opcode & 0x00FF;
+
+        case 0x4000: // SNE Vx, byte
+            pc += (V[x] != kk) ? 4 : 2;
+            break;
+
+        case 0x5000: // SE Vx, Vy
+            pc += (V[x] == V[y]) ? 4 : 2;
+            break;
+
+        case 0x6000: // LD Vx, byte
+            V[x] = kk;
             pc += 2;
             break;
-        }
-        case 0x7000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            V[x] += opcode & 0x00FF;
+
+        case 0x7000: // ADD Vx, byte
+            V[x] += kk;
             pc += 2;
             break;
-        }
-        case 0xA000:
-            I = opcode & 0x0FFF;
+
+        case 0x8000: // Arithmetic/logical
+            switch (n)
+            {
+            case 0x0:
+                V[x] = V[y];
+                break;
+            case 0x1:
+                V[x] |= V[y];
+                break;
+            case 0x2:
+                V[x] &= V[y];
+                break;
+            case 0x3:
+                V[x] ^= V[y];
+                break;
+            case 0x4: // ADD Vx, Vy with carry
+            {
+                uint16_t sum = V[x] + V[y];
+                V[0xF] = (sum > 0xFF) ? 1 : 0;
+                V[x] = sum & 0xFF;
+                break;
+            }
+            case 0x5: // SUB Vx, Vy
+                V[0xF] = (V[x] >= V[y]) ? 1 : 0;
+                V[x] -= V[y];
+                break;
+            case 0x6: // SHR Vx {, Vy}
+                V[0xF] = V[x] & 0x1;
+                V[x] >>= 1;
+                break;
+            case 0x7: // SUBN Vx, Vy
+                V[0xF] = (V[y] >= V[x]) ? 1 : 0;
+                V[x] = V[y] - V[x];
+                break;
+            case 0xE: // SHL Vx {, Vy}
+                V[0xF] = (V[x] & 0x80) >> 7;
+                V[x] <<= 1;
+                break;
+            }
             pc += 2;
             break;
-        case 0xD000:
+
+        case 0x9000: // SNE Vx, Vy
+            pc += (V[x] != V[y]) ? 4 : 2;
+            break;
+
+        case 0xA000: // LD I, addr
+            I = nnn;
+            pc += 2;
+            break;
+
+        case 0xB000: // JP V0, addr
+            pc = nnn + V[0];
+            break;
+
+        case 0xC000: // RND Vx, byte
+            V[x] = (rand() % 256) & kk;
+            pc += 2;
+            break;
+
+        case 0xD000: // DRW Vx, Vy, nibble
         {
-            uint8_t x = V[(opcode & 0x0F00) >> 8];
-            uint8_t y = V[(opcode & 0x00F0) >> 4];
-            uint8_t height = opcode & 0x000F;
+            uint8_t vx = V[x];
+            uint8_t vy = V[y];
+            uint8_t height = n;
             V[0xF] = 0;
 
             for (int row = 0; row < height; row++)
@@ -157,45 +224,83 @@ public:
                 {
                     if ((sprite & (0x80 >> col)) != 0)
                     {
-                        int px = (x + col) % 64;
-                        int py = (y + row) % 32;
+                        int px = (vx + col) % 64;
+                        int py = (vy + row) % 32;
                         if (display[py][px] == 1)
                             V[0xF] = 1;
                         display[py][px] ^= 1;
                     }
                 }
             }
+            pc += 2;
+            break;
+        }
 
-            pc += 2;
-            break;
-        }
-        case 0xC000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            V[x] = (rand() % 256) & (opcode & 0x00FF);
-            pc += 2;
-            break;
-        }
-        case 0xE000:
-        {
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            if ((opcode & 0x00FF) == 0x9E)
+        case 0xE000: // Key opcodes
+            switch (kk)
             {
-                if (keypad[V[x]])
-                    pc += 4;
-                else
-                    pc += 2;
-            }
-            else if ((opcode & 0x00FF) == 0xA1)
-            {
-                if (!keypad[V[x]])
-                    pc += 4;
-                else
-                    pc += 2;
+            case 0x9E: // SKP Vx
+                pc += (keypad[V[x]]) ? 4 : 2;
+                break;
+            case 0xA1: // SKNP Vx
+                pc += (!keypad[V[x]]) ? 4 : 2;
+                break;
             }
             break;
-        }
+
+        case 0xF000: // Misc
+            switch (kk)
+            {
+            case 0x07:
+                V[x] = delay_timer;
+                break;
+            case 0x0A: // LD Vx, K (wait for key press)
+            {
+                bool keyPressed = false;
+                for (int i = 0; i < 16; i++)
+                {
+                    if (keypad[i])
+                    {
+                        V[x] = i;
+                        keyPressed = true;
+                        break;
+                    }
+                }
+                if (!keyPressed)
+                    return; // do not advance pc
+                break;
+            }
+            case 0x15:
+                delay_timer = V[x];
+                break;
+            case 0x18:
+                sound_timer = V[x];
+                break;
+            case 0x1E:
+                I += V[x];
+                break;
+            case 0x29:
+                I = V[x] * 5;
+                break; // sprite location
+            case 0x33: // BCD
+                memory[I] = V[x] / 100;
+                memory[I + 1] = (V[x] / 10) % 10;
+                memory[I + 2] = V[x] % 10;
+                break;
+            case 0x55: // LD [I], V0..Vx
+                for (int i = 0; i <= x; i++)
+                    memory[I + i] = V[i];
+                break;
+            case 0x65: // LD V0..Vx, [I]
+                for (int i = 0; i <= x; i++)
+                    V[i] = memory[I + i];
+                break;
+            }
+            pc += 2;
+            break;
+
         default:
+            std::cerr << "Unknown opcode: " << std::hex << opcode << std::endl;
             pc += 2;
             break;
         }
@@ -216,11 +321,11 @@ int main()
 {
     Chip8 chip8;
     chip8.initialize();
-    chip8.loadProgram("pong.ch8");
+    chip8.loadProgram("airplane.ch8");
 
     const int SCALE = 10;
-    const int CYCLES_PER_FRAME = 60; // Cycle function needs to be executed this many times in a frame so it can actually show something on the screen
-    const int FRAME_DELAY_MS = 16;   // 60 FPS
+    const int CYCLES_PER_FRAME = 1; // Cycle function needs to be executed this many times in a frame so it can actually show something on the screen
+    const int FRAME_DELAY_MS = 16;  // 60 FPS
 
     // Makes the centered 64x32 window scaled by 10
 
@@ -229,13 +334,21 @@ int main()
                                           SDL_WINDOWPOS_CENTERED,
                                           64 * SCALE, 32 * SCALE,
                                           SDL_WINDOW_SHOWN);
-    // IDK NEXT
+
+    // SDL renderer(the arguments of a function are window,graphic driver(-1 for the next available),and use GPU if available)
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+    // Representation of a pixel
+
     SDL_Rect rect{0, 0, SCALE, SCALE};
     bool running = true;
+
+    // Event holder
+
     SDL_Event e;
+
+    // Keyboard
 
     const SDL_Keycode keymap[16] = {
         SDLK_x, SDLK_1, SDLK_2, SDLK_3,
@@ -246,6 +359,7 @@ int main()
     while (running)
     {
         // Event handling
+
         while (SDL_PollEvent(&e))
         {
             if (e.type == SDL_QUIT)
@@ -265,10 +379,11 @@ int main()
             chip8.emulateCycle();
 
         // Render display
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black
+        SDL_RenderClear(renderer);                      // CLS
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Puts black as the color
         for (int y = 0; y < 32; y++)
             for (int x = 0; x < 64; x++)
                 if (chip8.display[y][x])
